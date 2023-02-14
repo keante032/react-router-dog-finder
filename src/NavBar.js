6bb8f25fb7ac0c2baeac9dadfaf8a4e2d320d1ac
Kevin Templeton <templeton.kevin@gmail.com>
2023-02-14 15:43:17 -0600

    finished

---
+++ src/NavBar.js
@@ -0,0 +1,18 @@
+import {NavLink} from "react-router-dom"
+import "./NavBar.css"
+
+function NavBar({dogs}) {
+  const links = dogs.map(dog => (
+    <NavLink key={dog.name} to={`/dogs/${dog.name.toLowerCase()}`} >
+      {dog.name}
+    </NavLink>
+  ));
+  return (
+    <nav>
+      <NavLink exact to="/dogs">Home</NavLink>
+      {links}
+    </nav>
+  );
+}
+
+export default NavBar;
